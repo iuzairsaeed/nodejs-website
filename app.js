58d52a48ba1f52ff4573c9c3d29ccd28855414ff
Uzair Saeed <uzair.saeed27@gmail.com>
2020-03-22 21:22:10 +0500

    set middleware

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 // Core modules
 const http = require('http');
-
 // custom modules / files
-const routes = require('./routes')
+// const routes = require('./routes');
 
+const express = require('express');
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000)
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();
+});
+app.use((req, res, next) => {
+    console.log('In anoher middleware');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
