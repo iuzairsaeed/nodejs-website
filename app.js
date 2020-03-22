f58c55c3480cfc437207f62969fe2d94e1e6833e
Uzair Saeed <uzair.saeed27@gmail.com>
2020-03-23 00:57:12 +0500

    filtering path

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not Found</h1>');
+});
 
 // app.use('/', (req, res, next) => {
 //     console.log('This always Run');
--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/add-product', (req, res, next) => {
     console.log('In anoher middleware');
     res.send(
-        '<form action="/product" method="POST"><input type="text" name="title" placeholder="Product Title"><input type="submit" value="Submit"></form>'
+        '<form action="product" method="POST"><input type="text" name="title" placeholder="Product Title"><input type="submit" value="Submit"></form>'
     );
 });
 router.post('/product', (req, res, next) => {
