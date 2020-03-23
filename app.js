42a18ffc9c6656bbf0d1a1a50688c90128b48b71
Uzair Saeed <uzair.saeed27@gmail.com>
2020-03-23 20:36:03 +0500

    install view engine

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const rootDir = require('./util/path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 
@@ -24,7 +24,7 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,17 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
 
+// GET 
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
-router.post('/product', (req, res, next) => {
-    console.log(req.body);
+// POST
+router.post('/add-product', (req, res, next) => {
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
