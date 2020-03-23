98f98c66005fbb7f8368d30b8d8aaf626d30880f
Uzair Saeed <uzair.saeed27@gmail.com>
2020-03-23 18:20:28 +0500

    styling Html

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // Core modules
 const http = require('http');
+const path = require('path');
+
 // custom modules / files
 // const routes = require('./routes');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const rootDir = require('./util/path');
 
 const app = express();
 
@@ -22,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not Found</h1>');
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 // app.use('/', (req, res, next) => {
